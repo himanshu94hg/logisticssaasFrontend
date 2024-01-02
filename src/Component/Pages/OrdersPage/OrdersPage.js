2d997ae7e8c1e0e41bc943d140d687bbe0e53561
Shipease2 <himanshu.grover@shipease.in>
2024-01-02 17:11:37 +0530

    UI Updated

--- src/Component/Pages/OrdersPage/OrdersPage.js
+++ src/Component/Pages/OrdersPage/OrdersPage.js
@@ -1,22 +1,56 @@
-import React from 'react'
-import NavTabs from './navTabs/NavTabs'
-import HomeIcon from './Icons/HomeIcon'
-import './OrdersPage.css'
-import InternationalIcon from './Icons/InternationalIcon'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import Form from 'react-bootstrap/Form';
+import NavTabs from './navTabs/NavTabs';
+import './OrdersPage.css'
+
 
 const OrdersPage = () => {
+    const [selectedOption, setSelectedOption] = useState("Option 1");
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOptionSelect = (option) => {
+        setSelectedOption(option);
+        setIsOpen(false);
+    };
+
+    const toggleOptions = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
-            <section className='w-100 box-shadow shadow-sm p7 bg-light orders-menu-header'>
+            <section className='w-100 orders-menu-header'>
                 <div className='d-flex justify-content-between'>
-                    <Form.Select aria-label="Default select example">
-                        <option value="1">Domestic</option>
-                        <option value="2">International</option>
-                    </Form.Select>
+                <div className={`down-sliding-select ${isOpen ? "open" : ""}`} onClick={toggleOptions}>
+                    <div className="selected-option">
+                        {selectedOption || "Select an option"}
+                        <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} />
+                        
+                    </div>
+                    {isOpen && (
+                        <div className="options-container">
+                            <div
+                                className={`option ${selectedOption === "Option 1" ? "selected" : ""}`}
+                                onClick={() => handleOptionSelect("Option 1")}
+                            >
+                                Domestic
+                            </div>
+                            <div
+                                className={`option ${selectedOption === "Option 2" ? "selected" : ""}`}
+                                onClick={() => handleOptionSelect("Option 2")}
+                            >
+                                International
+                            </div>
+                        </div>
+                    )}
+                </div>
                     <div>new import sync</div>
                 </div>
             </section>
+
+            
 
 
             <NavTabs />
