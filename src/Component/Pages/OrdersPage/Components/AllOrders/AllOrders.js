6e9403843b5abba028d398e588095c225eca6f5f
Shipease2 <himanshu.grover@shipease.in>
2024-01-04 16:14:42 +0530

    All Orders Page Updated

--- src/Component/Pages/OrdersPage/Components/AllOrders/AllOrders.js
+++ src/Component/Pages/OrdersPage/Components/AllOrders/AllOrders.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import SearchIcon from '../../../../../assets/image/icons/search-icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AllOrders = () => {
 
     const [selectAll, setSelectAll] = useState(false);
     const [selectedRows, setSelectedRows] = useState([]);
+    const [backDrop, setBackDrop] = useState(false);
 
     // Sample data for the table
     const tableData = [
-        { id: 1, orderDetails: 'Details 1', customerDetails: 'Customer 1', packagingDetails: 'Packaging 1', payment: 'Payment 1', deliveryAddress: 'Address 1', courierPartner: 'Courier 1', status: 'Status 1', action: 'Action 1' },
-        { id: 2, orderDetails: 'Details 2', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
-        { id: 3, orderDetails: 'Details 2', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
-        { id: 4, orderDetails: 'Details 2', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
-        { id: 5, orderDetails: 'Details 2', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
-        { id: 6, orderDetails: 'Details 2', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
+        { id: 1, orderDetails: 'Details 1', orderNumber: '2763812736', timeStamp: '04 Jan 2024 | 11:26 AM', channelDetails: 'Amazon', viewProduct: 'view Product', customerDetails: 'Customer 1', packagingDetails: 'Packaging 1', payment: 'Payment 1', deliveryAddress: 'Address 1', courierPartner: 'Courier 1', status: 'Status 1', action: 'Action 1' },
+        { id: 2, orderDetails: 'Details 2', orderNumber: '2763812736', timeStamp: '04 Jan 2024 | 11:26 AM', channelDetails: 'Amazon', viewProduct: 'view Product', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
+        { id: 3, orderDetails: 'Details 2', orderNumber: '2763812736', timeStamp: '04 Jan 2024 | 11:26 AM', channelDetails: 'Amazon', viewProduct: 'view Product', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
+        { id: 5, orderDetails: 'Details 2', orderNumber: '2763812736', timeStamp: '04 Jan 2024 | 11:26 AM', channelDetails: 'Amazon', viewProduct: 'view Product', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
+        { id: 6, orderDetails: 'Details 2', orderNumber: '2763812736', timeStamp: '04 Jan 2024 | 11:26 AM', channelDetails: 'Amazon', viewProduct: 'view Product', customerDetails: 'Customer 2', packagingDetails: 'Packaging 2', payment: 'Payment 2', deliveryAddress: 'Address 2', courierPartner: 'Courier 2', status: 'Status 2', action: 'Action 2' },
         // Add more rows as needed
     ];
 
@@ -45,9 +46,21 @@ const AllOrders = () => {
         }
     };
 
+    const handleSidePanel = () => {
+        document.getElementById("sidePanel").style.right = "0"
+        setBackDrop(true)
+    }
+
+    // useEffect(() => {
+    //   first
+
+
+    // }, [])
+
+
     return (
         <section>
-            <div className="">
+            <div className="position-relative">
                 <div className="box-shadow shadow-sm p7 mb-3 filter-container">
                     <div class="search-container">
                         <label>
@@ -59,8 +72,8 @@ const AllOrders = () => {
                         <p className='font10'>Most Popular Search by COD, Prepaid, Yesterday, One Week, Last Month, Delivered, Cancel order </p>
                     </div>
                     <div className='button-container'>
-                        <button className='btn main-button'></button>
-                        <button className='btn main-button'></button>
+                        <button className='btn main-button me-2' onClick={handleSidePanel}>Advanced Filters</button>
+                        <button className='btn main-button'>Report</button>
                     </div>
                 </div>
 
@@ -96,20 +109,76 @@ const AllOrders = () => {
                                             onChange={() => handleSelectRow(row.id)}
                                         />
                                     </td>
-                                    <td>{row.orderDetails}</td>
-                                    <td>{row.customerDetails}</td>
-                                    <td>{row.packagingDetails}</td>
-                                    <td>{row.payment}</td>
-                                    <td>{row.deliveryAddress}</td>
-                                    <td>{row.status}</td>
-                                    <td>{row.action}</td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className='cell-inside-box'>
+                                            <p>{row.orderNumber}</p>
+                                            <p>{row.timeStamp}</p>
+                                            <p>{row.channelDetails}</p>
+                                            <p>{row.viewProduct}</p>
+                                        </div>
+                                    </td>
                                 </tr>
                             </React.Fragment>
                         ))}
                     </tbody>
                 </table>
+
+                <div id='sidePanel' className="side-panel">
+                    <div className='sidepanel-closer'><FontAwesomeIcon icon="fa-solid fa-chevron-right" /></div>
+                </div>
+
+                <div className={`backdrop ${backDrop ? 'd-block' : 'd-none'}`}></div>
+
             </div>
-        </section>
+        </section >
     );
 };
 
