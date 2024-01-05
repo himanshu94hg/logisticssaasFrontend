e52f3de187e22311adcd7a54919e9ba43997ba3a
Shipease2 <himanshu.grover@shipease.in>
2024-01-05 17:01:27 +0530

    All orders Page Updated

--- src/Component/Pages/OrdersPage/Components/AllOrders/SidePanel.js
+++ src/Component/Pages/OrdersPage/Components/AllOrders/SidePanel.js
@@ -2,13 +2,17 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-const SidePanel = () => {
+const SidePanel = (props) => {
     return (
         <>
             <div id='sidePanel' className="side-panel">
-                <div className='sidepanel-closer'>
+                <div id='sidepanel-closer' onClick={props.CloseSidePanel}>
                     <FontAwesomeIcon icon={faChevronRight} />
                 </div>
+                <section className='sidepanel-header'>
+                    <h4>Explore Additional Filters</h4>
+                    <p>Fine-Tune Your Search</p>
+                </section>
             </div>
         </>
     )
