cc2b49353574e762cde7e9915b8ddd5e8befa382
Shipease2 <suneel.verma@shipease.in>
2024-01-09 14:52:35 +0530

    integrate api in deleverd and shipment tab

--- src/Component/Pages/ShipmentsPage/Components/RTOShipment/RTOShipment.js
+++ src/Component/Pages/ShipmentsPage/Components/RTOShipment/RTOShipment.js
@@ -46,10 +46,10 @@ const RTOShipment = () => {
 
     useEffect(() => {
         axios
-            .get('http://35.154.133.143/api/v1/allorderdetail/') // Replace with your API endpoint
+            .get('http://35.154.133.143/api/v1/ndrshipment/') // Replace with your API endpoint
             .then(response => {
                 console.log('Data is data:', response.data);
-                setAllOrders(response.data);
+                setAllOrders(response.data.shipment_data);
             })
             .catch(error => {
                 console.error('Error:', error);
