e6a59aace9b8a1c36b54b880746cface885732fb
sanjeev <sanjeev.kumar@shipease.in>
2024-02-16 17:18:27 +0530

    login & logout code added

--- src/App.js
+++ src/App.js
@@ -30,91 +30,74 @@ import ChannelIntegrationForm from './Component/Pages/IntegrationsPage/Component
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import WalletRechargeComponent from './Component/Pages/WalletRechargeComponent/WalletRechargeComponent';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { channelsIntegrationPattern, couriersIntegrationPattern, dailyPrefrencesPattern, generateApiKeyPattern, indexPattern, loginPattern, mergeOrdersPattern, omsIntegrationPattern, ordersPattern, reassignOrdersPattern, shipmentsPattern, socailPagePattern, splitOrdersPattern } from "./Routes";
-import { useDispatch } from "react-redux";
-import { USER_DATA_ACTION } from "./redux/saga/constant";
 import Cookies from "js-cookie";
-import axios from "axios";
 
 
 function App() {
+
   const [WalletRecharge, setWalletRecharge] = useState(false)
+  const navigate = useNavigate();
+  const [tokenExists, setTokenExists] = useState(false); // State to store token existence
 
-  const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NTk3ODE2LCJpYXQiOjE3MDc5OTMwMTYsImp0aSI6IjlkMDA2NDdhNThlMTQ2ZTg4YTY5NDk5NTBkMDYzODIwIiwidXNlcl9pZCI6Mn0.U_LfFwstyHksDD0J1U2oULKxIhbK3D0Htoj1-Bdaqos";
+
+  useEffect(() => {
+    const token = Cookies.get('access_token');
+    setTokenExists(!!token);
+  }, [tokenExists, navigate]);
+
+  useEffect(() => {
+    if (!tokenExists) {
+      navigate(loginPattern);
+    } else {
+      navigate(indexPattern);
+    }
+  }, [tokenExists, navigate]);
 
 
-  useEffect(()=>{
-    Cookies.set('token', authToken);
-  },[])
-
-  const dispatch = useDispatch()
-
-  const seller_id="3"
-
-  const handleecllick = () => {
-    // dispatch({ type: USER_DATA_ACTION,payload:{seller_id} })
-
-    // console.log("object")
-
-    // const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3OTkyNDk2LCJpYXQiOjE3MDczODc2OTYsImp0aSI6IjEzODE0YWE2ZjE2ZTQyNzk5NzhhNzAwZmY0MTM1YTZhIiwidXNlcl9pZCI6Mn0.neIQZnSs3vkyMxm0QrfIOpu_RTjDNz5j3vF-OPNNXTA";
-
-      // axios
-      //     .get(`http://65.2.38.87:8088/core-api/features/support-tickets/`, {
-      //         headers: {
-      //             Authorization: `Bearer ${authToken}`
-      //         }
-      //     })
-      //     .then(response => {
-      //         console.log('Data is data:', response.data.results);
-      //     })
-      //     .catch(error => {
-      //         console.error('Error:', error);
-      //     });
-
-  }
+  console.log(tokenExists, "tokenExists");
 
   return (
     <>
 
       <div className="container p-0 m-0" style={{ display: "flex" }}>
         <div className="rightContainer">
-          <div>
-            {/* <button onClick={() => handleecllick()}>Clicssssssk</button> */}
-            <Header WalletRecharge={WalletRecharge} setWalletRecharge={setWalletRecharge} />
-          </div>
 
-          <Router>
+          {tokenExists && <>
+            <Header WalletRecharge={WalletRecharge} setWalletRecharge={setWalletRecharge} />
             <Sidebar />
-            <Routes>
-              <Route path={indexPattern} element={<Dashboard />} />
-              <Route path={loginPattern} element={<LoginPage />} />
-              <Route path={reassignOrdersPattern} element={<AllOrders />} />
-              <Route path={mergeOrdersPattern} element={<AllOrders />} />
-              <Route path={splitOrdersPattern} element={<AllOrders />} />
-              <Route path={ordersPattern} element={<OrdersPage />} />
-              <Route path={shipmentsPattern} element={<ShipmentsPage />} />
-              <Route path={dailyPrefrencesPattern} element={<DailyPrefrences />} />
-              <Route path={channelsIntegrationPattern} element={<ChannelsIntegration />} />
-              <Route path={omsIntegrationPattern} element={<OMSIntegration />} />
-              <Route path={couriersIntegrationPattern} element={<CouriersIntegration />} />
-              <Route path={generateApiKeyPattern} element={<APIIntegration />} />
-              <Route path={socailPagePattern} element={<OtherIntegration />} />
-              <Route path="/IndiaMapp" element={<IndiaMapp />} />
-              <Route path="/create-order" element={<CreateOrderFlow />} />
-              <Route path="/billing" element={<BillingPage />} />
-              <Route path="/weight-reconciliation" element={<WeightRecoPage />} />
-              <Route path="/MIS" element={<MISPage />} />
-              <Route path="/customer" element={<CustomerPage />} />
-              <Route path="/customer-support" element={<CustomerSupportPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/help-articles" element={<HelpArticles />} />
-              <Route path="/manage-warehouses" element={<ManageWarehouse />} />
-              <Route path="/add-pickup-address" element={<AddWarehouse />} />
-              <Route path="/shipping-rates" element={<ShippingRates />} />
-              <Route path="/shopify-integration" element={<ChannelIntegrationForm />} />
-            </Routes>
-          </Router>
+          </>}
+          {/* <Router> */}
+          <Routes>
+            <Route path={indexPattern} element={<Dashboard />} />
+            <Route path={loginPattern} element={<LoginPage />} />
+            <Route path={reassignOrdersPattern} element={<AllOrders />} />
+            <Route path={mergeOrdersPattern} element={<AllOrders />} />
+            <Route path={splitOrdersPattern} element={<AllOrders />} />
+            <Route path={ordersPattern} element={<OrdersPage />} />
+            <Route path={shipmentsPattern} element={<ShipmentsPage />} />
+            <Route path={dailyPrefrencesPattern} element={<DailyPrefrences />} />
+            <Route path={channelsIntegrationPattern} element={<ChannelsIntegration />} />
+            <Route path={omsIntegrationPattern} element={<OMSIntegration />} />
+            <Route path={couriersIntegrationPattern} element={<CouriersIntegration />} />
+            <Route path={generateApiKeyPattern} element={<APIIntegration />} />
+            <Route path={socailPagePattern} element={<OtherIntegration />} />
+            <Route path="/IndiaMapp" element={<IndiaMapp />} />
+            <Route path="/create-order" element={<CreateOrderFlow />} />
+            <Route path="/billing" element={<BillingPage />} />
+            <Route path="/weight-reconciliation" element={<WeightRecoPage />} />
+            <Route path="/MIS" element={<MISPage />} />
+            <Route path="/customer" element={<CustomerPage />} />
+            <Route path="/customer-support" element={<CustomerSupportPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/help-articles" element={<HelpArticles />} />
+            <Route path="/manage-warehouses" element={<ManageWarehouse />} />
+            <Route path="/add-pickup-address" element={<AddWarehouse />} />
+            <Route path="/shipping-rates" element={<ShippingRates />} />
+            <Route path="/shopify-integration" element={<ChannelIntegrationForm />} />
+          </Routes>
+          {/* </Router> */}
         </div>
       </div>
       <WalletRechargeComponent WalletRecharge={WalletRecharge} setWalletRecharge={setWalletRecharge} />
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/store';
 import { Provider } from 'react-redux'
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 ReactDOM.render(
   <>
     <Provider store={store}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </Provider>
   </>,
   document.getElementById('root')
