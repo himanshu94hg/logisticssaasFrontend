8bbcc7af3e5cb8e69066560a8d9d98adec72bc9f
sanjeev <sanjeev.kumar@shipease.in>
2024-02-16 13:17:52 +0530

    css code fixing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/store';
 import { Provider } from 'react-redux'
+import './index.css';
+
+
 
 ReactDOM.render(
   <>
