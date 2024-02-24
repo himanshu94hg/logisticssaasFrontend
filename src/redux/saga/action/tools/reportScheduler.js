9bb1f2b9a601965e85e613fa3539bf198845c766
sanjeev <sanjeev.kumar@shipease.in>
2024-02-24 16:27:38 +0530

    code added

---
+++ src/redux/saga/action/tools/reportScheduler.js
@@ -0,0 +1,33 @@
+import axios from "../../../../axios/index"
+import {  REPORT_SCHEDULER_ACTION } from "../../constant/tools";
+import { call, put, takeLatest } from "@redux-saga/core/effects";
+import { API_URL, BASE_URL_ORDER } from "../../../../axios/config";
+import {  GET_REPORT_SCHEDULER_DATA } from "../../../constants/tools";
+
+async function reportSchedulerAPI(data) {
+    let listData = axios.request({
+        method: "GET",
+        url:  `${BASE_URL_ORDER}${API_URL.GET_REPORTS_SCHEDULER}`,
+        data: data
+    });
+    return listData
+}
+
+function* reportSchedulerAction(action) {
+    let { payload, reject } = action;
+    try {
+        let response = yield call(reportSchedulerAPI, payload);
+        if (response.status === 200) {
+            yield put({ type: GET_REPORT_SCHEDULER_DATA, payload: response })
+        }
+        else {
+
+        }
+    } catch (error) {
+        if (reject) reject(error);
+    }
+}
+
+export function* getReportSchedulerWatcher(data) {
+    yield takeLatest(REPORT_SCHEDULER_ACTION, reportSchedulerAction);
+}
