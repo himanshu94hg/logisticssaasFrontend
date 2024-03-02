28049d617c67917737038cc602fcc8412f7ffd32
sanjeev <sanjeev.kumar@shipease.in>
2024-03-02 10:52:14 +0530

    code added

--- src/axios/config.js
+++ src/axios/config.js
@@ -10,5 +10,6 @@ export const API_URL = {
     GET_USERS:"/orders-api/orders/",
     GET_RATE_CARD:"/core-api/shipping/get-seller-rate/",
     GET_RATE_CALCULATOR:"/core-api/seller/tools/rate-calculator/",
+    GET_BILLING_URLW:"/core-api/features/billing/passbook/",
     GET_BILLING_URL:"/users"
 };
