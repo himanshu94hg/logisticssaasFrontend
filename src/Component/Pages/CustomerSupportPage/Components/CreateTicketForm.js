bdd3c75f165716415472b5e4ee7358743c28fd64
sanjeev <sanjeev.kumar@shipease.in>
2024-03-05 16:42:00 +0530

    create ticket code added

--- src/Component/Pages/CustomerSupportPage/Components/CreateTicketForm.js
+++ src/Component/Pages/CustomerSupportPage/Components/CreateTicketForm.js
@@ -5,12 +5,15 @@ import { toast } from 'react-toastify';
 import { awsAccessKey } from '../../../../config';
 import React, { useState, useEffect } from 'react';
 import { getFileData, uploadImageData } from '../../../../awsUploadFile';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 // Reusable FormInput component
-const FormInput = ({ label, mandatory, type, value, onChange, options, name, fileInput, customClass }) => (
+const FormInput = ({ label, mandatory, type, value, onChange, options, name, fileInput, customClass, selectFile, clearFile }) => (
 
   <div className='ticket-form-row'>
+    {console.log(selectFile, "this is selecyed file")}
     <label>{label} <span className='text-danger'>{mandatory}</span></label>
     {type === 'select' ? (
       <select className='select-field' name={name} value={value} onChange={onChange}>
@@ -24,7 +27,14 @@ const FormInput = ({ label, mandatory, type, value, onChange, options, name, fil
     ) : type === 'textarea' ? (
       <textarea className={`input-field text-field ${customClass}`} rows="4" value={value} name={name} onChange={onChange} />
     ) : type === 'file' ? (
-      <input className='input-field choose-file-container' type={type} onChange={onChange} name={name} id={fileInput} />
+      <div className="file-input-container">
+        <input className='input-field choose-file-container' type={type} onChange={onChange} name={name} id={fileInput} />
+        {selectFile && (
+          <span style={{ position: "relative", right: "-89%", top: "-24px", cursor: "pointer" }}>
+            <FontAwesomeIcon icon={faTimesCircle} className="clear-file-icon text-danger" onClick={clearFile} size="lg" /> {/* Using size="2x" for extra-large */}
+          </span>
+        )}
+      </div>
     ) : (
       <input className={`input-field x ${customClass}`} type={type} value={value} onChange={onChange} name={name} />
     )}
@@ -39,6 +49,8 @@ const CreateTicketForm = (props) => {
   const [isLoading, setIsLoading] = useState(false)
   const [allCatagery, setAllCatagery] = useState([])
   const [allSubCatagry, setAllSubCatagry] = useState([])
+  const [selectFile, setSelectFile] = useState(false)
+  const [fileObj, setFileObj] = useState(null)
 
   const [ticketData, setTicketData] = useState({
     category: 1,
@@ -57,6 +69,13 @@ const CreateTicketForm = (props) => {
     value: subcategory.id,
     label: subcategory.name,
   }));
+
+
+  const clearFile = () => {
+    setSelectFile(false);
+    setFileError("");
+    document.getElementById("fileInput").value = ""; // Reset the file input
+  };
 
   useEffect(() => {
     axios
@@ -86,7 +105,7 @@ const CreateTicketForm = (props) => {
           if (response.data.length > 0) {
             setTicketData(prevData => ({
               ...prevData,
-              sub_category: response.data[0].id // Assuming 'id' is the property for subcategory id
+              sub_category: response.data[0].id
             }));
           }
         })
@@ -105,7 +124,39 @@ const CreateTicketForm = (props) => {
     }))
   }
 
-
+  useEffect(() => {
+    if (ticketData.escalate_image !== "" && fileObj) {
+      const postTicketData = async () => {
+        try {
+          const response = await axios.post('http://dev.shipease.in:8081/core-api/features/support-tickets/', ticketData, {
+            headers: {
+              'Authorization': `Bearer ${authToken}`,
+              'Content-Type': 'application/json',
+            },
+          });
+          if (response.status === 201) {
+            toast.success("Ticket created successfully")
+            setTicketData({
+              category: 1,
+              sub_category: null,
+              awb_number: "",
+              description: "",
+              escalate_image: ""
+            })
+            setSelectFile(false);
+            setFileObj(null)
+            props?.setStatus(!props.status)
+            props.setNewTicket(false)
+            document.getElementById("fileInput").value = "";
+          }
+        } catch (error) {
+          toast.error(error?.response?.data?.detail)
+        }
+      };
+      postTicketData(); 
+    }
+  }, [ticketData.escalate_image, fileObj]);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     const validationErrors = {};
@@ -116,68 +167,70 @@ const CreateTicketForm = (props) => {
       validationErrors.description = "Remarks is required!";
     }
     setErrors(validationErrors)
-
+  
     if (Object.keys(validationErrors).length === 0) {
-      try {
-        const response = await axios.post('http://dev.shipease.in:8081/core-api/features/support-tickets/', ticketData, {
-          headers: {
-            'Authorization': `Bearer ${authToken}`,
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-
-        if (response.status === 201) {
-          toast.success("Ticket created successfully")
-          setTicketData({
-            category: 1,
-            sub_category: null,
-            awb_number: "",
-            description: "",
-            escalate_image: ""
-          })
-          props?.setStatus(!props.status)
-          props.setNewTicket(false)
-          document.getElementById("fileInput").value = "";
-        } else {
-          toast.error("Something went wrong!")
+      if (fileObj) {
+        try {
+          const responseData = await getFileData(fileObj?.name.replace(/\s/g, ""))
+          const awsUrl = responseData.data.url.url
+          const formData = new FormData();
+          formData.append('key', responseData.data.url.fields.key);
+          formData.append('file', fileObj);
+          formData.append('AWSAccessKeyId', awsAccessKey);
+          formData.append('policy', responseData.data.url.fields.policy);
+          formData.append('signature', responseData.data.url.fields["x-amz-signature"]);
+          await uploadImageData(awsUrl, formData); // No need to check additionalData.status
+          const imageUrl = responseData?.data?.url?.url + fileObj?.name.replace(/\s/g, "")
+          setTicketData(prev => ({
+            ...prev,
+            escalate_image: imageUrl
+          }));
         }
-      } catch (error) {
-        toast.error("Something went wrong!")
+        catch (error) {
+          toast.error(error?.response?.data?.detail)
+        }
+      }
+      else {
+        try {
+        const response=  await axios.post('http://dev.shipease.in:8081/core-api/features/support-tickets/', ticketData, {
+            headers: {
+              'Authorization': `Bearer ${authToken}`,
+              'Content-Type': 'application/json',
+            },
+          });
+          if (response.status === 201) {
+            toast.success("Ticket created successfully")
+            setTicketData({
+              category: 1,
+              sub_category: null,
+              awb_number: "",
+              description: "",
+              escalate_image: ""
+            })
+            
+            props?.setStatus(!props.status)
+            props.setNewTicket(false)
+          }
+
+        } catch (error) {
+          toast.error(error?.response?.data?.detail)
+        }
       }
     }
-
   };
-
+  
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
     const fileSizeInMB = parseFloat((file?.size / (1024 * 1024)).toFixed(2));
+    if (file != undefined) {
+      setSelectFile(true);
+      setFileObj(file)
+    }
     if (fileSizeInMB > 2) {
       setFileError("File should be less than 3 mb")
     }
     else {
       setFileError("")
-      try {
-        const responseData = await getFileData(e.target.files[0].name.replace(/\s/g, ""));
-        responseData.status === 200 && setIsLoading(true);
-        const awsUrl = responseData.data.url.url
-        const formData = new FormData();
-        formData.append('key', responseData.data.url.fields.key);
-        formData.append('file', e.target.files[0]);
-        formData.append('AWSAccessKeyId', awsAccessKey);
-        formData.append('policy', responseData.data.url.fields.policy);
-        formData.append('signature', responseData.data.url.fields["x-amz-signature"]);
-        const additionalData = await uploadImageData(awsUrl, formData);
-        if (additionalData?.status == 204) {
-          setIsLoading(false);
-          const imageUrl = responseData?.data?.url?.url + e.target.files[0]?.name.replace(/\s/g, "")
-          setTicketData(prev => ({
-            ...prev,
-            [e.target.name]: imageUrl
-          }));
-        }
-      } catch (error) {
-        console.error('Error handling file change:', error);
-      }
     }
   };
 
@@ -223,7 +276,9 @@ const CreateTicketForm = (props) => {
           type="file"
           fileInput="fileInput"
           name="escalate_image"
+          selectFile={selectFile}
           onChange={handleFileChange}
+          clearFile={clearFile}
         />
         {fileError != '' && <span className='error-text'>{fileError}</span>}
       </div>
