4cefb99c3ad378adedb87e7875f0280de637d3be
Nitesh Singh <nitesh.singh.@shipease.in>
2024-03-13 15:31:48 +0530

    Courier Allocation Integration

--- src/redux/saga/action/tools/courierAllocation.js
+++ src/redux/saga/action/tools/courierAllocation.js
@@ -1,7 +1,8 @@
 import axios from "../../../../axios/index"
-import { COURIER_ALLOCATION_ACTION,  } from "../../constant/tools";
-import { call,  takeLatest } from "@redux-saga/core/effects";
+import { COURIER_ALLOCATION_ACTION,COURIER_ALLOCATION_PARTNER_ACTION,COURIER_ALLOCATION_PARTNER_POST_ACTION } from "../../constant/tools";
+import { call,put,takeLatest } from "@redux-saga/core/effects";
 import { API_URL, BASE_URL_CORE } from "../../../../axios/config";
+import {GET_COURIER_ALLOCATION_DATA,GET_COURIER_ALLOCATION_POST_DATA} from "../../../constants/tools";
 
 async function courierAllocationAPI(data) {
     let listData = axios.request({
@@ -10,6 +11,25 @@ async function courierAllocationAPI(data) {
     });
     return listData
 }
+
+async function courierAllocationGetAPI(data) {
+    let getData = axios.request({
+        method: "GET",
+        url: `${BASE_URL_CORE}${API_URL.GET_COURIER_ALLOCATION_PARTNER}`,
+    });
+    return getData
+}
+
+async function courierAllocationPostAPI(data) {
+    console.log("GET_COURIER_POST_ALLOCATION ",data);
+    try {
+        const response = await axios.post(`${BASE_URL_CORE}${API_URL.GET_COURIER_POST_ALLOCATION}`, data);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 function* courierAllocationAction(action) {
     let { payload, reject } = action;
     try {
@@ -22,6 +42,35 @@ function* courierAllocationAction(action) {
     }
 }
 
+function* courierAllocationGetAction(action) {
+    let { payload, reject } = action;
+    try {
+        let response = yield call(courierAllocationGetAPI, payload);
+        if (response.status === 200) {
+            yield put({ type: GET_COURIER_ALLOCATION_DATA, payload: response?.data })
+        }
+    } catch (error) {
+        if (reject) reject(error);
+    }
+}
+
+function* courierAllocationPostAction(action) {
+    console.log(action,"Action All Data")
+    let { payload, reject } = action;
+    try {
+        let response = yield call(courierAllocationPostAPI, payload);
+        if (response) {
+            yield put({ type: GET_COURIER_ALLOCATION_POST_DATA, payload: response });
+        }
+    } catch (error) {
+        if (reject) reject(error);
+    }
+}
+
+
 export function* courierAllocationWatcher() {
+    console.log("Payload Post Data Watcher");
     yield takeLatest(COURIER_ALLOCATION_ACTION, courierAllocationAction);
+    yield takeLatest(COURIER_ALLOCATION_PARTNER_ACTION, courierAllocationGetAction);
+    yield takeLatest(COURIER_ALLOCATION_PARTNER_POST_ACTION, courierAllocationPostAction);
 }
