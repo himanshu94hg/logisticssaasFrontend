9cad098672d68d5ae261dc1e36499bf4af37ce4b
Nitesh Singh <nitesh.singh.@shipease.in>
2024-03-15 18:30:13 +0530

    preview button

--- src/Component/Pages/ToolsPage/Components/RateCalculatorPage/RateCalculatorPage.js
+++ src/Component/Pages/ToolsPage/Components/RateCalculatorPage/RateCalculatorPage.js
@@ -76,6 +76,7 @@ const RateCalculatorPage = () => {
       setOrderField(false);
     }
   }
+  
   // const handleChange = (e) => {
   //   const scaleDataName = e.target.name;
   //   if (scaleDataName === "length" || scaleDataName === "breadth" || scaleDataName === "height") {
