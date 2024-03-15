9e44c377a9ddf5ec6f5c47ddcfe1228db16058b1
Nitesh Singh <nitesh.singh.@shipease.in>
2024-03-15 13:04:46 +0530

    preview button

--- src/Component/Pages/CustomerSupportPage/Components/ViewTicketSlider.js
+++ src/Component/Pages/CustomerSupportPage/Components/ViewTicketSlider.js
@@ -83,7 +83,7 @@ const ViewTicketSlider = ({ viewId, ViewTicketInfo, setViewTicketInfo, }) => {
     }
   };
   function extractFileName(fullPath) {
-    // Split the fullPath string by '/' to get an array of parts
+    // Split the fullPath string by '/' to get an array of parts.
     const parts = fullPath.split('/');
     // Get the last part of the array, which should be the file name
     const fileName = parts[parts.length - 1];
