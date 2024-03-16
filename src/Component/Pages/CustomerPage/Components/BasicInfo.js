f7150700b7a2d7a14e6f5c78ed548a34e3c8b6ad
Nitesh Singh <nitesh.singh.@shipease.in>
2024-03-16 15:08:38 +0530

    billing

--- src/Component/Pages/CustomerPage/Components/BasicInfo.js
+++ src/Component/Pages/CustomerPage/Components/BasicInfo.js
@@ -14,6 +14,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import Modal from "react-bootstrap/Modal";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+
 const BasicInfo = ({ activeTab }) => {
   const [errors, setErrors] = useState({});
   const [logoError, setLogoError] = useState("");
