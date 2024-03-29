6f88088c8bf4b76b6f2cf92092070794af7d2acc
Sanjeev <sanjeev.kumar@shipease.in>
2024-03-29 18:28:19 +0530

    code added

--- src/Component/Pages/Dashboard/Components/Orders/TotalOrderInfo.js
+++ src/Component/Pages/Dashboard/Components/Orders/TotalOrderInfo.js
@@ -25,20 +25,22 @@ function TotalOrderInfo() {
                     <div className="CardIconContainer icon-bg">
                       <img src={iconOrders} alt="iconOrders" width={24} />
                     </div>
-                    <p className="font14 text-gray m-0 ws-nowrap">Total Orders</p>
-                    <h3 className="font20 title-text p-y bold-600 m0">{orderCount?.today_order}</h3>
+                    <div>
+                      <p className="font14 text-gray m-0 ws-nowrap">Total Orders</p>
+                      <h3 className="font20 title-text p-y bold-600 m0">{orderCount?.today_order}</h3>
+                    </div>
                   </div>
                   <div className="col-2">
                     <HiTrendingUp className="trending-icon" />
                   </div>
-                  <div className="card-footer1">
-                  <span className="text-red font13 pt20 bold-600 d-block text-end">
-                    { 0 } %
-                  </span>
-                  <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
-                    this month
-                  </p>
-                 </div>
+                  {/* <div className="card-footer1">
+                    <span className="text-red font13 pt20 bold-600 d-block text-end">
+                      {0} %
+                    </span>
+                    <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
+                      this month
+                    </p>
+                  </div> */}
                 </div>
               </div>
               <div className="col-12">
@@ -58,22 +60,24 @@ function TotalOrderInfo() {
                       <img src={iconDelivery} alt="iconDelivery" width={24} />
 
                     </div>
-                    <p className="font14 text-gray m-0 ws-nowrap">Cancel Order</p>
-                    <h3 className="font20 title-text p-y bold-600 m0">
-                      {orderCount?.cancel_order}
-                    </h3>
+                    <div>
+                      <p className="font14 text-gray m-0 ws-nowrap">Cancel Order</p>
+                      <h3 className="font20 title-text p-y bold-600 m0">
+                        {orderCount?.cancel_order}
+                      </h3>
+                    </div>
                   </div>
                   <div className="col-2">
                     <HiTrendingUp className="trending-icon" />
                   </div>
-                  <div className="card-footer1">
-                  <span className="text-red font13 pt20 bold-600 d-block text-end">
-                    { 0 } %
-                  </span>
-                  <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end ">
-                    this month
-                  </p>
-                 </div>
+                  {/* <div className="card-footer1">
+                    <span className="text-red font13 pt20 bold-600 d-block text-end">
+                      {0} %
+                    </span>
+                    <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end ">
+                      this month
+                    </p>
+                  </div> */}
                 </div>
               </div>
               <div className="col-12">
@@ -92,20 +96,22 @@ function TotalOrderInfo() {
                     <div className="CardIconContainer icon-bg">
                       <img src={iconDelivery} alt="iconDelivery" width={24} />
                     </div>
-                    <p className="font14 text-gray m-0 ws-nowrap">Yet To Pick</p>
-                    <h3 className="font20 title-text p-y bold-600 m0">{orderCount?.yet_to_pick}</h3>
+                    <div>
+                      <p className="font14 text-gray m-0 ws-nowrap">Yet To Pick</p>
+                      <h3 className="font20 title-text p-y bold-600 m0">{orderCount?.yet_to_pick}</h3>
+                    </div>
                   </div>
                   <div className="col-2">
                     <HiTrendingUp className="trending-icon" />
                   </div>
-                  <div className="card-footer1">
-                  <span className="text-red font13 pt20 bold-600 d-block text-end">
-                    { 0 } %
-                  </span>
-                  <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
-                    this month
-                  </p>
-                 </div>
+                  {/* <div className="card-footer1">
+                    <span className="text-red font13 pt20 bold-600 d-block text-end">
+                      {0} %
+                    </span>
+                    <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
+                      this month
+                    </p>
+                  </div> */}
                 </div>
               </div>
               <div className="col-12">
@@ -124,22 +130,24 @@ function TotalOrderInfo() {
                     <div className="CardIconContainer icon-bg">
                       <img src={iconRTO} alt="iconRTO" width={24} />
                     </div>
-                    <p className="font14 text-gray m-0 ws-nowrap">Reverse Orders</p>
-                    <h3 className="font20 title-text p-y bold-600 m0">
-                      {orderCount?.reverse_order}
-                    </h3>
+                    <div>
+                      <p className="font14 text-gray m-0 ws-nowrap">Reverse Orders</p>
+                      <h3 className="font20 title-text p-y bold-600 m0">
+                        {orderCount?.reverse_order}
+                      </h3>
+                    </div>
                   </div>
                   <div className="col-2">
                     <HiTrendingDown className="trending-icon" />
                   </div>
-                  <div className="card-footer1">
-                  <span className="text-red font13 pt20 bold-600 d-block text-end">
-                    { 0 } %
-                  </span>
-                  <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
-                    this month
-                  </p>
-                 </div>
+                  {/* <div className="card-footer1">
+                    <span className="text-red font13 pt20 bold-600 d-block text-end">
+                      {0} %
+                    </span>
+                    <p className="text-xs text-gray font12 m0 text-gray-600 ws-nowrap text-end">
+                      this month
+                    </p>
+                  </div> */}
                 </div>
               </div>
               <div className="col-12">
