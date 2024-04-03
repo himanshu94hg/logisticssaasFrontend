a4b8be1c5ce3b27aef18a0d66bfad6253fdf3e83
Nitesh Singh <nitesh.singh.@shipease.in>
2024-04-04 04:18:59 +0530

    shipment

--- src/redux/saga/action/tools/courierAllocation.js
+++ src/redux/saga/action/tools/courierAllocation.js
@@ -188,7 +188,7 @@ async function courierAllocationRuleStatusAPI(data) {
     console.log("All Status Rules",data);
     let listData = axios.request({
         method: "GET",
-        url: `${BASE_URL_CORE}${API_URL.GET_COURIER_ALLOCATION_STATUS_RULE}?preference_id=${data?.id}&status=${data?.status}`,
+        url: `${BASE_URL_CORE}${API_URL.GET_COURIER_ALLOCATION_STATUS_RULE}?preference_id=${data?.id}&status=${data?.togglestatus}`,
         data:data
     });
     return listData
