0a867cb7859eaf3c5f78d07a56c72ae814d0a7dc
Nitesh Singh <nitesh.singh.@shipease.in>
2024-04-04 04:20:04 +0530

    shipment

--- src/Component/Pages/ToolsPage/Components/CourierAllocationPage/SetPreferenceRules.js
+++ src/Component/Pages/ToolsPage/Components/CourierAllocationPage/SetPreferenceRules.js
@@ -48,7 +48,6 @@ const SetPreferenceRules = () => {
     }, []);
 
     const addRuleRow = () => {
-        console.log("Hit Add");
         setRulePanel(true);
         setEditingRuleId(null);
         setRuleName('');
