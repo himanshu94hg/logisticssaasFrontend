4739c9abe41192ac4d25bb4a90a7b9a5c2d1398c
Nitesh Singh <nitesh.singh.@shipease.in>
2024-04-05 16:13:25 +0530

    shipment

--- src/Component/Pages/OrdersPage/Components/BulkActionsComponent/BulkActionsComponent.js
+++ src/Component/Pages/OrdersPage/Components/BulkActionsComponent/BulkActionsComponent.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import './BulkActionsComponent.css'
-import { useDispatch } from 'react-redux'
 
 const BulkActionsComponent = ({ activeTab, selectedRows }) => {
     const dispatch = useDispatch();
