57935d723c8abe612246d30267c349ba36c04ff4
Nitesh Singh <nitesh.singh.@shipease.in>
2024-04-12 18:22:52 +0530

    clone order

--- src/Component/Pages/ToolsPage/Components/ZoneMappingPop/ZoneMappingPop.js
+++ src/Component/Pages/ToolsPage/Components/ZoneMappingPop/ZoneMappingPop.js
@@ -1,70 +1,49 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './ZoneMappingPop.css'
-import { PiExport } from "react-icons/pi";
-import { saveAs } from 'file-saver';
-import * as XLSX from 'xlsx';
+import React, { useEffect, useRef, useState } from 'react';
+import './ZoneMappingPop.css';
+import { PiExport } from 'react-icons/pi';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 
-const ZoneMappingPop = ({ ZoneMapping, setZoneMapping }) => {
+const ZoneMappingPop = ({ setZoneMapping }) => {
   const dispatch = useDispatch();
   const popRef = useRef(null);
-  const [zoneData, setZoneData] = useState([])
-  const [pincode, setPincode] = useState("")
-  const [zoneStatus, setZoneStatus] = useState(false)
-  const { zoneMapping } = useSelector(state => state?.toolsSectionReducer)
-  const { pathName } = useSelector(state => state?.authDataReducer)
-  const tempPath=pathName+new Date()
+  const [pincode, setPincode] = useState('');
+  const [zoneStatus, setZoneStatus] = useState(false);
+  const { pathName } = useSelector(state => state?.authDataReducer);
 
   useEffect(() => {
-    if (pathName==="Zone Mapping") {
-      setZoneStatus(true)
-      setZoneMapping(true)
+    if (pathName === 'Zone Mapping') {
+      setZoneStatus(true);
+      setZoneMapping(true);
     }
-  }, [tempPath])
+  }, [pathName]);
 
   useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popRef.current && !popRef.current.contains(event.target)) {
+        setZoneMapping(false);
+        setZoneStatus(false);
+      }
+    };
+
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+  }, [setZoneMapping]);
 
-  const handleClickOutside = (event) => {
-    if (popRef.current && !popRef.current.contains(event.target)) {
-      setZoneMapping(false);
-      setZoneStatus(false)
-    }
-  };
-
-  const data = [
-    {
-      name: "Sanjeev"
-    }
-  ]
   const exportToExcel = async () => {
     try {
-      const response = await dispatch({ type: "ZONE_MAPPING_ACTION", payload: pincode });
-      // const exportData = data; 
-      // const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-      // const fileExtension = '.xlsx';
-      // const ws = XLSX.utils.json_to_sheet(exportData);
-      // const wb = { Sheets: { zoneMapping: ws }, SheetNames: ['zoneMapping'] };
-      // const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-      // const excelData = new Blob([excelBuffer], { type: fileType });
-      // saveAs(excelData, 'exported_data' + fileExtension);
+      const response = await dispatch({ type: 'ZONE_MAPPING_ACTION', payload: pincode });
     } catch (error) {
-      // toast.error('Error exporting data:', error);
+      toast.error('Error exporting data: ' + error.message);
     }
-
   };
-
-
 
   return (
     <>
-      <section  ref={popRef}  className={`zone-mapping-container ${zoneStatus ? '' : 'd-none'}`}>
+      <section ref={popRef} className={`zone-mapping-container ${zoneStatus ? '' : 'd-none'}`}>
         <h4>Zone Mapping</h4>
         <form action="">
           <div className='d-flex mt-4 gap-4 align-items-end justify-content-between'>
@@ -73,13 +52,13 @@ const ZoneMappingPop = ({ ZoneMapping, setZoneMapping }) => {
               <input className='input-field' type="text" placeholder='6 Digits Pick-up Area Pincode' onChange={(e) => setPincode(e.target.value)} />
             </label>
             <button type='button' className='btn main-button' onClick={exportToExcel}>
-              <span className='rotate-180'><PiExport fontSize={25} onClick={()=>setZoneMapping(false)} /></span> Export
+              <span className='rotate-180'><PiExport fontSize={25} /></span> Export
             </button>
           </div>
         </form>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default ZoneMappingPop
+export default ZoneMappingPop;
