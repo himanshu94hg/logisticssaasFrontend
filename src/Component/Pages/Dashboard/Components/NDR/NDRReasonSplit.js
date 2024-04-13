609c1cfa8cf60badae2c390cefa5d70c7bb0dd85
Sanjeev <sanjeev.kumar@shipease.in>
2024-04-13 13:35:09 +0530

    code  added

--- src/Component/Pages/Dashboard/Components/NDR/NDRReasonSplit.js
+++ src/Component/Pages/Dashboard/Components/NDR/NDRReasonSplit.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import ReactApexChart from 'react-apexcharts';
 
 const NDRPieChart = () => {
-
     const ndrSplit =useSelector(state=>state?.dashboardNdrReducer?.splitStatus)
     console.log("NDR RESPONSE Data",ndrSplit)
     const seriesData = ndrSplit?.map(item => item?.count) || [];
