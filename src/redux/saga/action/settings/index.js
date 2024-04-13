317f0aa9788856783c06e3319ef84cf188986c87
Sanjeev <sanjeev.kumar@shipease.in>
2024-04-13 10:51:21 +0530

    edit warehouse

--- src/redux/saga/action/settings/index.js
+++ src/redux/saga/action/settings/index.js
@@ -1,10 +1,12 @@
 import axios from "../../../../axios/index"
 import { call, put, takeLatest } from "@redux-saga/core/effects";
 import { API_URL, BASE_URL_CORE, BASE_URL_DUMMY } from "../../../../axios/config";
-import { DELETE_WAREHOUSE_ACTION, EDIT_WAREHOUSE_ACTION, MAKE_WAREHOUSE_DEFAULT_ACTION } from "../../constant/settings";
-import { GET_DEFAULT_WAREHOUSE_UPDATE } from "../../../constants/settings";
+import { DELETE_WAREHOUSE_ACTION, EDIT_WAREHOUSE_ACTION, GET_WAREHOUSE_DETAILS_ACTION, MAKE_WAREHOUSE_DEFAULT_ACTION } from "../../constant/settings";
+import { GET_DEFAULT_WAREHOUSE_UPDATE, GET_WAREHOUSE_DETAILS_DATA } from "../../../constants/settings";
 import { toast } from "react-toastify";
 
+
+// SETTINGS_MAKE_WAREHOUSE_DEFAULT_API
 async function makeWareHouseDefaultApi(data) {
     let listData = axios.request({
         method: "PUT",
@@ -13,7 +15,6 @@ async function makeWareHouseDefaultApi(data) {
     });
     return listData;
 }
-
 function* makeWareHouseDefaultAction(action) {
     let { payload, reject } = action;
     try {
@@ -28,7 +29,6 @@ function* makeWareHouseDefaultAction(action) {
     }
 }
 
-
 //SETTINGS_DELETE_WAREHOUSE_API
 async function deleteWarehouseApi(data) {
     let listData = axios.request({
@@ -38,7 +38,6 @@ async function deleteWarehouseApi(data) {
     });
     return listData;
 }
-
 function* deleteWarehouseAction(action) {
     let { payload, reject } = action;
     try {
@@ -53,12 +52,40 @@ function* deleteWarehouseAction(action) {
         if (reject) reject(error);
     }
 }
+
+// SETTINGS_GET_WAREHOUSE_DETAILS_API
+async function warehouseDetailsApi(data) {
+    console.log(data,"this is a code data")
+    let listData = axios.request({
+        method: "GET",
+        url: `${BASE_URL_CORE}${API_URL.SETTINGS_EDIT_WAREHOUSE_API}${data}/`,
+        // data: data?.formData
+    });
+    return listData;
+}
+
+function* warehouseDetailsAction(action) {
+    let { payload, reject } = action;
+    try {
+        let response = yield call(warehouseDetailsApi, payload);
+        if (response.status === 200) {
+            // toast("Warehouse Edited successfully!")
+            yield put({ type: GET_WAREHOUSE_DETAILS_DATA, payload: response?.data })
+            
+        }
+        else {
+        }
+    } catch (error) {
+        if (reject) reject(error);
+    }
+}
+
 // SETTINGS_EDIT_WAREHOUSE_API
 async function editWarehouseApi(data) {
     let listData = axios.request({
-        method: "DELETE",
-        url: `${BASE_URL_CORE}${API_URL.SETTINGS_DELETE_WAREHOUSE_API}${data}/`,
-        //data: data
+        method: "PUT",
+        url: `${BASE_URL_CORE}${API_URL.SETTINGS_EDIT_WAREHOUSE_API}${data?.wareHouseId}/`,
+        data: data?.formData
     });
     return listData;
 }
@@ -67,7 +94,7 @@ function* editWarehouseAction(action) {
     let { payload, reject } = action;
     try {
         let response = yield call(editWarehouseApi, payload);
-        if (response.status === 204) {
+        if (response.status === 200) {
             toast("Warehouse Edited successfully!")
             yield put({ type: GET_DEFAULT_WAREHOUSE_UPDATE, payload: response?.status })
         }
@@ -81,6 +108,7 @@ function* editWarehouseAction(action) {
 export function* getSettingsWatcher() {
     yield takeLatest(MAKE_WAREHOUSE_DEFAULT_ACTION,makeWareHouseDefaultAction);
     yield takeLatest(DELETE_WAREHOUSE_ACTION,deleteWarehouseAction);
+    yield takeLatest(GET_WAREHOUSE_DETAILS_ACTION,warehouseDetailsAction);
     yield takeLatest(EDIT_WAREHOUSE_ACTION,editWarehouseAction);
  
 }
