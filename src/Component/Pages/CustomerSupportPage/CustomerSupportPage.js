16aaf6e810bf8ba370365f21676956bf3007d1d2
Pintu6201 <pintu.prasad@shipease.in>
2024-04-19 17:47:19 +0530

    Code added

--- src/Component/Pages/CustomerSupportPage/CustomerSupportPage.js
+++ src/Component/Pages/CustomerSupportPage/CustomerSupportPage.js
@@ -152,6 +152,7 @@ const CustomerSupportPage = () => {
         .catch(error => {
           toast.error("Something went wrong!")
         });
+        setCurrentPage(1)
     }
   }
 
@@ -168,7 +169,7 @@ const CustomerSupportPage = () => {
         })
         .catch(error => {
           toast.error("Something went wrong!")
-        });
+        }); 
     }else if (activeTab === 'openTickets') {
       axios.get(`https://dev.shipease.in/core-api/features/support-tickets/?status=Open&page_size=${20}&page=${1}&courier_status${activeTab==="openTickets" ?'':activeTab}`, {
         headers: {
