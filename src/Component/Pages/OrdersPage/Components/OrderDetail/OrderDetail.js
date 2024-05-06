48c1b621491aac4ef10c0dc015e6400e0e1f0324
Shipease2 <himanshu.grover@shipease.in>
2024-05-06 17:17:36 +0530

    UI Updated

--- src/Component/Pages/OrdersPage/Components/OrderDetail/OrderDetail.js
+++ src/Component/Pages/OrdersPage/Components/OrderDetail/OrderDetail.js
@@ -7,19 +7,21 @@ const OrderDetail = () => {
     return (
         <>
             <section className='box-shadow shadow-sm p10 order-detials-page h-100'>
-                <div className='d-flex justify-content-between align-items-center'>
-                    <div className='d-flex flex-column'>
-                        <h3>Order ID: #Order@1</h3>
-                        <p className='order-Status-box'>Ready To Ship</p>
-                    </div>
-                    <div className='d-flex gap-2'>
-                        <button className='btn main-button-outline'>Export</button>
-                        <button className='btn main-button'>Ship Now</button>
+                <div className='d-flex flex-column'>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <div className='d-flex flex-column'>
+                            <h3>Order ID: #Order@1</h3>
+                            <p className='order-Status-box'>Ready To Ship</p>
+                        </div>
+                        <div className='d-flex gap-2'>
+                            <button className='btn main-button-outline'>More</button>
+                            <button className='btn main-button-outline'>Export</button>
+                            <button className='btn main-button'>Ship Now</button>
+                        </div>
                     </div>
                 </div>
-
-                <div>
-                    <h4>Order Details</h4>
+                <div className='mt-5'>
+                    <h5>Order Details</h5>
                     <hr />
                 </div>
 
