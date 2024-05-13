86600235417bae890cadfdd56553d16fbfecc674
Shipease2 <himanshu.grover@shipease.in>
2024-05-13 17:17:47 +0530

    UI Updated

--- src/Component/Pages/EarnAndGrowPages/ReferAndEarnPage/ReferAndEarnPage.js
+++ src/Component/Pages/EarnAndGrowPages/ReferAndEarnPage/ReferAndEarnPage.js
@@ -4,12 +4,15 @@ import SendInvitaionIcon from './Icons/SendInvitaionIcon'
 import RegistrationIcon from './Icons/RegistrationIcon'
 import RechargeIcon from './Icons/RechargeIcon'
 import FreeCashIcon from './Icons/FreeCashIcon'
+import BookOrder from './Icons/BookOrder'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 const ReferAndEarnPage = () => {
     return (
         <>
             <section className='box-shadow shadow-sm p10 rae-page'>
-                <h2>Refer And Earn</h2>
+                <h3>Refer And Earn</h3>
                 <p>Invite you friends. If they Ship their first order, you and your friend will get coins to redeem!</p>
 
                 <div className='referal-steps'>
@@ -29,18 +32,38 @@ const ReferAndEarnPage = () => {
                     </div>
                     <div className='step-content'>
                         <div className='step-image-sec'>
-                            <RechargeIcon />
+                            <BookOrder />
                         </div>
-                        <h5>Recharge</h5>
-                        <p>They will recharge their wallet.</p>
+                        <h5>Recharge and Book Order</h5>
+                        <p>They will recharge their wallet and book their first order with us.</p>
                     </div>
                     <div className='step-content'>
                         <div className='step-image-sec'>
                             <FreeCashIcon />
                         </div>
                         <h5>Enjoy Free Cash!</h5>
-                        <p></p>
+                        <p>You and your friend will get cash credit in your wallet!</p>
                     </div>
+                </div>
+
+                <h4 className='mt-5'>Invite your friends</h4>
+                <p>Insert your friends' addresses and send them invitations to join Shipease!</p>
+                <div className='rae-invite-sec'>
+                    <label htmlFor="">
+                        <input className='input-field' type="text" />
+                        <button type='button' title='Send Invite'><FontAwesomeIcon icon={faPaperPlane} /></button>
+                    </label>
+                </div>
+
+                <h4 className='mt-5'>Share The Referal Link</h4>
+                <p>You can also share your referral link by copying and sending it or sharing it on your social media.</p>
+
+                <div className='rae-social-sec'>
+                    <label htmlFor="">
+                        <input type="text" value="shipease.in/relento-8724669673" />
+                        <button>Copy</button>
+                    </label>
+                    <button></button>
                 </div>
             </section>
         </>
