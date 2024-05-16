64f42c1f968a64d81cc4863380ab1585cefabef9
Shipease2 <himanshu.grover@shipease.in>
2024-05-16 17:13:22 +0530

    UI Updated

---
+++ src/Component/Pages/SettingsPage/components/UserRoleManage/UserRoleManage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './UserRoleManage.css'
+
+const UserRoleManage = () => {
+    return (
+        <div>UserRoleManage</div>
+    )
+}
+
+export default UserRoleManage
