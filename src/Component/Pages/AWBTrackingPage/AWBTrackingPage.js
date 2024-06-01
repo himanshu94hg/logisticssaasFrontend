f0bb4fb885580bcdd749f199e9aa2968c9ae8984
Shipease2 <himanshu.grover@shipease.in>
2024-06-01 15:27:31 +0530

    UI Updated

--- src/Component/Pages/AWBTrackingPage/AWBTrackingPage.js
+++ src/Component/Pages/AWBTrackingPage/AWBTrackingPage.js
@@ -2,6 +2,8 @@ import { faChevronRight, faCircleCheck, faCircleDot } from '@fortawesome/free-so
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './AWBTrackingPage.css'
+import TrackingIcon from './TrackingIcon'
+import TrackingDone from './TrackingDone'
 
 const AWBTrackingPage = ({ setOrderTracking }) => {
 
@@ -20,65 +22,72 @@ const AWBTrackingPage = ({ setOrderTracking }) => {
             <section className='tracking-body'>
                 <ul>
                     <li className='active'>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleDot} />
+                        <div className='track-icon'>
+                            <TrackingIcon />
                         </div>
                         <div>
                             <h4>Order Delivered</h4>
+                            <p>Status: Pending</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Order Received at Delivery Center</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Order In Transit</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Order Picked up</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Pickup Requested</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Order Booked</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
                     <li>
-                        <div>
-                            <FontAwesomeIcon icon={faCircleCheck} />
+                        <div className='track-icon'>
+                            <TrackingDone />
                         </div>
                         <div>
                             <h4>Order Created</h4>
+                            <p>Status: Done</p>
                             <p>1 June 2024 || 12:00 PM</p>
                         </div>
                     </li>
---
+++ src/Component/Pages/AWBTrackingPage/TrackingDone.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const TrackingDone = () => {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width={24}
+                height={24}
+                x={0}
+                y={0}
+                viewBox="0 0 510 510"
+                style={{ enableBackground: "new 0 0 512 512" }}
+                xmlSpace="preserve"
+                className="hovered-paths"
+            >
+                <g>
+                    <path
+                        d="m150.45 206.55-35.7 35.7L229.5 357l255-255-35.7-35.7-219.3 219.3-79.05-79.05zM459 255c0 112.2-91.8 204-204 204S51 367.2 51 255 142.8 51 255 51c20.4 0 38.25 2.55 56.1 7.65l40.801-40.8C321.3 7.65 288.15 0 255 0 114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255h-51z"
+                        fill="#000000"
+                        opacity={1}
+                        data-original="#000000"
+                        className="hovered-path"
+                    />
+                </g>
+            </svg>
+
+        </>
+    )
+}
+
+export default TrackingDone
