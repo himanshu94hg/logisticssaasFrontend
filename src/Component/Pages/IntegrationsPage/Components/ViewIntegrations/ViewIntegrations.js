c71b2750318c10155a85355c7f14c04f365b2196
Shipease2 <himanshu.grover@shipease.in>
2024-06-06 18:57:15 +0530

    UI Updated

--- src/Component/Pages/IntegrationsPage/Components/ViewIntegrations/ViewIntegrations.js
+++ src/Component/Pages/IntegrationsPage/Components/ViewIntegrations/ViewIntegrations.js
@@ -3,7 +3,9 @@ import React from 'react'
 const ViewIntegrations = () => {
     return (
         <>
-
+            <div style={{ height: '20vh' }} className='d-flex w-100 align-items-center justify-content-center'>
+                <h1>Under Development</h1>
+            </div>
         </>
     )
 }
