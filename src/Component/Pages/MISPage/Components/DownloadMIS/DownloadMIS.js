c0d7c41cbab1b18beb7b94a7515d8b89aa44d190
Nitesh Singh <nitesh.singh.@shipease.in>
2024-06-08 09:28:23 +0530

    Fixed MIS Download issue

--- src/Component/Pages/MISPage/Components/DownloadMIS/DownloadMIS.js
+++ src/Component/Pages/MISPage/Components/DownloadMIS/DownloadMIS.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import SearchIcon from '../../../../../assets/image/icons/search-icon.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 import axios from 'axios';
 import { toast } from 'react-toastify';
@@ -14,52 +12,29 @@ import { BASE_URL_ORDER } from '../../../../../axios/config';
 import { customErrorFunction } from '../../../../../customFunction/errorHandling';
 
 const DownloadMIS = ({ activeTab }) => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const [selectAll, setSelectAll] = useState(false);
     const [selectedRows, setSelectedRows] = useState([]);
-    const { misDownloadData } = useSelector(state => state?.misSectionReducer)
-    const [searchValue, setSearchValue] = useState("")
+    const { misDownloadData } = useSelector(state => state?.misSectionReducer);
+    const [searchValue, setSearchValue] = useState("");
     const [misDownload, setmisDownload] = useState([]);
-    let authToken = Cookies.get("access_token")
+    const authToken = Cookies.get("access_token");
 
     const [totalItems, setTotalItems] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage, setItemsPerPage] = useState(20);
 
     useEffect(() => {
-        dispatch({ type: "MIS_DOWNLOAD_ACTION", payload: { "itemsPerPage": itemsPerPage, "currentPage": currentPage } })
-    }, [dispatch, activeTab, itemsPerPage, currentPage])
+        dispatch({ type: "MIS_DOWNLOAD_ACTION", payload: { itemsPerPage, currentPage } });
+    }, [dispatch, activeTab, itemsPerPage, currentPage]);
 
     useEffect(() => {
         if (misDownloadData?.results !== null && misDownloadData !== undefined) {
-            setmisDownload(misDownloadData?.results)
-            setTotalItems(misDownloadData?.count)
+            setmisDownload(misDownloadData?.results);
+            setTotalItems(misDownloadData?.count);
         }
-    }, [misDownloadData])
+    }, [misDownloadData]);
 
-    const [orders, setAllOrders] = useState([
-        {
-            id: 1,
-            name: 'Bulk Labels',
-            type: 'Label',
-            status: 'In Progress',
-            requested_date: '24 Mar 2024',
-            completed_date: '',
-            d_file: 'labels.pdf'
-        },
-        {
-            id: 2,
-            name: 'Bulk invoice',
-            type: 'Invoice',
-            status: 'Done',
-            requested_date: '24 Mar 2024',
-            completed_date: '24 Mar 2024',
-            d_file: 'invoice.pdf'
-        }
-        // Add more dummy data as needed
-    ]);
-
-    // Handler for "Select All" checkbox
     const handleSelectAll = () => {
         setSelectAll(!selectAll);
         if (!selectAll) {
@@ -69,17 +44,13 @@ const DownloadMIS = ({ activeTab }) => {
         }
     };
 
-    // Handler for individual checkbox
     const handleSelectRow = (orderId) => {
         const isSelected = selectedRows.includes(orderId);
-
         if (isSelected) {
             setSelectedRows(selectedRows.filter(id => id !== orderId));
         } else {
             setSelectedRows([...selectedRows, orderId]);
         }
-
-        // Check if all rows are selected, then select/deselect "Select All"
         if (selectedRows.length === misDownload.length - 1 && isSelected) {
             setSelectAll(false);
         } else {
@@ -90,29 +61,58 @@ const DownloadMIS = ({ activeTab }) => {
     const handleSearch = () => {
         axios.get(`${BASE_URL_ORDER}/orders-api/mis/downloads/?q=${searchValue}`, {
             headers: {
-                Authorization: `Bearer ${authToken}`
-            }
+                Authorization: `Bearer ${authToken}`,
+            },
         }).then(response => {
-            console.log(response, "this is response")
-            setmisDownload(response.data.results)
-            // setSearchValue("")
-        })
-            .catch(error => {
-                customErrorFunction(error)
-            });
+            setmisDownload(response.data.results);
+        }).catch(error => {
+            customErrorFunction(error);
+        });
     };
 
     useEffect(() => {
         if (activeTab) {
             setSearchValue("");
         }
-    }, [activeTab])
+    }, [activeTab]);
 
     const handleReset = () => {
-        setSearchValue("")
-        setItemsPerPage(20)
-        setmisDownload(misDownloadData?.results)
-    }
+        setSearchValue("");
+        setItemsPerPage(20);
+        setmisDownload(misDownloadData?.results);
+    };
+
+    const handleDownload = (url, filename) => {
+        if (!url) {
+            toast.error("File download URL is not available or the report is still pending.");
+            return;
+        }
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/pdf',
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.blob();
+            } else {
+                throw new Error('Failed to download file');
+            }
+        })
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => console.error('Error:', error));
+    };
 
     return (
         <section className='position-relative downloads-mis'>
@@ -121,25 +121,17 @@ const DownloadMIS = ({ activeTab }) => {
                     <div className="search-container">
                         <label style={{ width: '500px' }}>
                             <input className='input-field' type="text" placeholder="Search your downloads" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-                            <button onClick={() => handleSearch()}>
+                            <button onClick={handleSearch}>
                                 <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </button>
                         </label>
-                        <button className='btn main-button-outline' onClick={() => handleReset()}><RxReset className='align-text-bottom' /> Reset</button>
+                        <button className='btn main-button-outline' onClick={handleReset}><RxReset className='align-text-bottom' /> Reset</button>
                     </div>
-                   
                 </div>
                 <div className='table-container'>
-                    <table className=" w-100">
+                    <table className="w-100">
                         <thead className="sticky-header">
                             <tr className="table-row box-shadow">
-                                {/*<th style={{ width: '1%' }}>
-                                    <input
-                                        type="checkbox"
-                                        checked={selectAll}
-                                        onChange={handleSelectAll}
-                                    />
-    </th>*/}
                                 <th style={{ width: '25%' }}>Name</th>
                                 <th>Type</th>
                                 <th>Status</th>
@@ -154,47 +146,42 @@ const DownloadMIS = ({ activeTab }) => {
                                 <React.Fragment key={row.id}>
                                     {index > 0 && <tr className="blank-row"><td></td></tr>}
                                     <tr className='table-row box-shadow'>
-                                        {/*<td className='checkbox-cell'>
-                                            <input
-                                                type="checkbox"
-                                                checked={selectedRows.includes(row.id)}
-                                                onChange={() => handleSelectRow(row.id)}
-                                            />
-                            </td>*/}
                                         <td>
-                                            {/* Name */}
                                             <div className='cell-inside-box'>
                                                 {row.report_title}
                                             </div>
                                         </td>
                                         <td>
-                                            {/* Type */}
                                             <div className='cell-inside-box'>
                                                 {row.report_type}
                                             </div>
                                         </td>
                                         <td>
-                                            {/* Status */}
                                             <div className='cell-inside-box'>
                                                 {row.report_status}
                                             </div>
                                         </td>
                                         <td>
-                                            {/* Request Date */}
                                             <div className='cell-inside-box'>
-                                                {moment(row.requested_at).format("DD MMM YYYY")}
+                                                {moment(row.created_at).format("DD MMM YYYY, hh:mm:ss A")}
                                             </div>
                                         </td>
                                         <td>
-                                            {/* Completed Date */}
                                             <div className='cell-inside-box'>
-                                                {moment(row.completed_at).format("DD MMM YYYY")}
+                                                {row.finished_at ? moment(row.finished_at).format("DD MMM YYYY, hh:mm:ss A") : 'N/A'}
                                             </div>
                                         </td>
                                         <td>
-                                            {/* Action */}
-                                            <button className='btn main-button'><FontAwesomeIcon icon={faDownload} /></button>
+                                            {row.report_status !== "pending" ? (
+                                                <button className='btn main-button' onClick={() => handleDownload(row.report_download_url, row.report_title)}>
+                                                    <FontAwesomeIcon icon={faDownload} />
+                                                </button>
+                                            ) : (
+                                                null
+                                            )}
                                         </td>
+
+
                                     </tr>
                                 </React.Fragment>
                             ))}
