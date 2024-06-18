1cfbf2176821e39b414d3e9ff115ca7ccd094e68
Shipease2 <himanshu.grover@shipease.in>
2024-06-18 11:53:00 +0530

    SellerNotificationsPage UI Updated

--- src/Component/Pages/SettingsPage/components/SellerNotificationsPage/SellerNotificationsPage.js
+++ src/Component/Pages/SettingsPage/components/SellerNotificationsPage/SellerNotificationsPage.js
@@ -8,32 +8,39 @@ const SellerNotificationsPage = () => {
         <>
             <div>
                 <div className='d-flex justify-content-between align-items-center'>
-                    <h4>Notification Settings</h4>
+                    <h4 className='mb-0'>Notification Settings</h4>
                     <button className='btn main-button-outline' onClick={() => Navigate(-1)}><MdOutlineKeyboardBackspace className='align-text-bottom' /> Go back</button>
                 </div>
-                <section className='box-shadow shadow-sm p10 mt-3'>
-                    <h5>Add Email and Mobile No to receive Business related notifications.</h5>
+                <section className='box-shadow shadow-sm p10 mt-3 py-4'>
+                    <h6>Add Email and Mobile No to receive Business related notifications</h6>
                     <p>For example: COD Remittance etc. You can add multiple email ids and mobile nos.</p>
-                    <label>
-                        Email ID
-                        <input className='input-field' type="text" />
-                    </label>
-                    <label>
-                        Mobile No.
-                        <input className='input-field' type="text" />
-                    </label>
-                    <h5>Add Email and Mobile No to receive Operations related notifications.</h5>
+                    <div className='d-flex gap-3 mt-2 w-100'>
+                        <label>
+                            Email ID
+                            <input className='input-field' type="text" />
+                        </label>
+                        <label>
+                            Mobile No.
+                            <input className='input-field' type="text" />
+                        </label>
+                    </div>
+
+                    <h6 className='mt-5'>Add Email and Mobile No to receive Operations related notifications</h6>
                     <p>For example: Label, Order Invoice, Manifest, NDR etc. You can add multiple email ids and mobile nos.</p>
-                    <label>
-                        Email ID
-                        <input className='input-field' type="text" />
-                    </label>
-                    <label>
-                        Mobile No.
-                        <input className='input-field' type="text" />
-                    </label>
-                    <button className='btn main-button'>Save</button>
+                    <div className='d-flex gap-3 mt-2 w-100'>
+                        <label>
+                            Email ID
+                            <input className='input-field' type="text" />
+                        </label>
+                        <label>
+                            Mobile No.
+                            <input className='input-field' type="text" />
+                        </label>
+                    </div>
                 </section>
+                <div className='mt-4 d-flex justify-content-end'>
+                    <button className='btn main-button'>Save</button>
+                </div>
             </div>
         </>
     )
