ffee09430eab286b8ef694ed47b7f85133353b39
Shipease2 <himanshu.grover@shipease.in>
2024-06-27 11:16:19 +0530

    single-ship-container UI Updated

--- src/Component/Pages/OrdersPage/Components/AllOrders/SingleShipPopReassign.js
+++ src/Component/Pages/OrdersPage/Components/AllOrders/SingleShipPopReassign.js
@@ -26,11 +26,11 @@ const SingleShipPopReassign = ({ reassignCard, SingleShipReassign, setSingleShip
         return result;
     };
 
-    const handleSubmit = (option,shipCharge) => {
-        console.log(paymentCard,shipCharge,option,'this is a reassign data')
+    const handleSubmit = (option, shipCharge) => {
+        console.log(paymentCard, shipCharge, option, 'this is a reassign data')
 
         if (paymentCard?.balance - shipCharge.toFixed(2) > paymentCard?.tolerance_limit) {
-            console.log(paymentCard,shipCharge,option,'this is a reassign data>>>>>>>>>>>>')
+            console.log(paymentCard, shipCharge, option, 'this is a reassign data>>>>>>>>>>>>')
             dispatch({ type: "REASSIGN_SHIP_DATA_ACTION", payload: { "courier": option, "order_id": orderId } });
             setShipingData(true);
         } else {
@@ -55,15 +55,15 @@ const SingleShipPopReassign = ({ reassignCard, SingleShipReassign, setSingleShip
 
     return (
         <section className={`single-ship-container ${SingleShipReassign ? 'open' : ''}`}>
-            <div className='d-flex justify-content-between p10 align-items-center'>
-                {/* <h4 className='mb-0'>Choose Shipping Partner</h4>
+            {/* <div className='d-flex justify-content-between p10 align-items-center'>
+                <h4 className='mb-0'>Choose Shipping Partner</h4>
                 <button
                     onClick={handleClose}
                     className='btn close-button'
                 >
                     <FontAwesomeIcon icon={faTimes} />
-                </button> */}
-            </div>
+                </button>
+            </div> */}
             <div className='ss-container-main'>
                 {/* Iterate over ship options and render details */}
                 {reassignCard && reassignCard?.map((option, index) => (
@@ -112,7 +112,7 @@ const SingleShipPopReassign = ({ reassignCard, SingleShipReassign, setSingleShip
                             </p>
                         </div>
                         <div className='d-flex flex-column gap-2 align-items-end'>
-                        <button className='btn main-button' onClick={() => handleSubmit(option.partner_keyword,option?.rate + option?.cod_charge + option?.early_cod_charge)}>Ship Now</button>
+                            <button className='btn main-button' onClick={() => handleSubmit(option.partner_keyword, option?.rate + option?.cod_charge + option?.early_cod_charge)}>Ship Now</button>
                             <p><span>EDD: <strong>{option?.estimate_days} days</strong></span></p>
                         </div>
                         {option?.is_recommended &&
