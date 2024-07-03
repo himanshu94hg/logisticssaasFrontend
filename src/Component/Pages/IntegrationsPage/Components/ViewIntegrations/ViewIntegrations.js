eda58488f0faf4fb4d8cc9e0072f309823e401c9
Shipease007 <nitesh.singh@shipease.in>
2024-07-03 16:06:09 +0530

    fixed

--- src/Component/Pages/IntegrationsPage/Components/ViewIntegrations/ViewIntegrations.js
+++ src/Component/Pages/IntegrationsPage/Components/ViewIntegrations/ViewIntegrations.js
@@ -6,10 +6,29 @@ import OMSView from './Components/OMSView';
 import OthersView from './Components/OthersView';
 import CourierView from './Components/CourierView';
 import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 const ViewIntegrations = () => {
     const location = useLocation()
+    const dispatch = useDispatch()
     const [activeTab, setActiveTab] = useState("Channel")
+    const [channelData,setChannelData] = useState([]);
+
+    const channelGetCard = useSelector(state => state?.channelSectionReducer?.channelGetCard)
+
+    useEffect(() => {
+        if(activeTab === "Channel")
+        {
+            dispatch({ type: "CHANNEL_GET_DATA_ACTION" });
+        }
+    }, [dispatch,activeTab])
+
+    useEffect(() => {
+        if (channelGetCard?.count > 0) {
+            setChannelData(channelGetCard?.results);
+        }
+    }, [channelGetCard?.results]);
+
 
     useEffect(() => {
         if (location.state?.tabState) {
@@ -23,7 +42,7 @@ const ViewIntegrations = () => {
 
             <div className='view-integrations-page'>
                 <div className={`${activeTab === "Channel" ? "d-block" : "d-none"}`}>
-                    <ChannelsView />
+                    <ChannelsView channelData={channelData}/>
                 </div>
 
                 <div className={`${activeTab === "OMS" ? "d-block" : "d-none"}`}>
--- src/redux/saga/action/channel/index.js
+++ src/redux/saga/action/channel/index.js
@@ -1,8 +1,8 @@
 import axios from "../../../../axios/index"
 import { call, put, takeLatest } from "@redux-saga/core/effects";
 import { API_URL, BASE_URL_DUMMY } from "../../../../axios/config";
-import { CHANNEL_SET_DATA_ACTION } from "../../constant/channel";
-import { SET_CHANNEL_DATA } from "../../../constants/channel";
+import { CHANNEL_SET_DATA_ACTION,CHANNEL_GET_DATA_ACTION } from "../../constant/channel";
+import { SET_CHANNEL_DATA,GET_CHANNEL_DATA } from "../../../constants/channel";
 
 
 
@@ -29,7 +29,32 @@ function* channelSetFilesAction(action) {
     }
 }
 
+async function channelGetFileAPI(data) {
+    let listData = axios.request({
+        method: "GET",
+        url: `${BASE_URL_DUMMY}${API_URL.GET_CHANNEL_API_URL}`,
+        // data: data
+    });
+    return listData;
+}
 
-export function* getpaymentWatcher() {
+function* channelGetFilesAction(action) {
+    let { payload, reject } = action;
+    try {
+        let response = yield call(channelGetFileAPI, payload);
+        if (response.status === 200) {
+            yield put({ type: GET_CHANNEL_DATA, payload: response?.data })
+        }
+        else {
+        }
+    } catch (error) {
+        if (reject) reject(error);
+    }
+}
+
+
+
+export function* getChannelWatcher() {
     yield takeLatest(CHANNEL_SET_DATA_ACTION,channelSetFilesAction);
+    yield takeLatest(CHANNEL_GET_DATA_ACTION,channelGetFilesAction);
 }
