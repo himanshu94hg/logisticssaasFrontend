d5cad669f3a989ca9fd1fc4a8ae800d36621468a
sanjeev <sanjeev.kumar@shipease.in>
2024-07-08 19:13:16 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -219,21 +219,6 @@ function App() {
 
       />
 
-      {/* 
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-        wrapperStyle
-        wrapperClass
-      /> */}
-
-      {/* <MigrationNewsPop /> */}
-      {/* Same as */}
-
-      <LoaderScreen />
     </>
 
   );
