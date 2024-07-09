89cb625ca9278a2aa41e659d7f4330c035b223b0
Shipease2 <himanshu.grover@shipease.in>
2024-07-09 17:22:11 +0530

    Loader Screen Updated

--- src/Component/LoaderScreen/LoaderScreen.js
+++ src/Component/LoaderScreen/LoaderScreen.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 import './LoaderScreen.css'
-import { RotateSpinner } from 'react-spinners-kit';
+// import { RotateSpinner } from 'react-spinners-kit';
+// import loaderImage from '../../assets/image/loader-image.png'
 
-const LoaderScreen = ({LoaderRing}) => {
+const LoaderScreen = ({ LoaderRing }) => {
   return (
     <>
       {LoaderRing &&
         <>
           <div className="loader-screen">
-            <RotateSpinner size={100} color="#60a9eb" />
+            {/* <RotateSpinner size={100} color="#60a9eb" /> */}
+            {/* <img src={loaderImage} /> */}
+            <img src="./logo512.png" />
           </div>
           <div className="backdrop"></div>
         </>
