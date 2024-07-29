266ae2d4bc426d669d9236788e6f502cfb594ad2
Shipease2 <himanshu.grover@shipease.in>
2024-07-29 11:31:20 +0530

    API Logo Added in all Orders Tabs

--- src/Component/Pages/OrdersPage/Components/Processing/Processing.js
+++ src/Component/Pages/OrdersPage/Components/Processing/Processing.js
@@ -17,6 +17,7 @@ import storeHipImg from "../../../../../assets/image/integration/StoreHippoLogo.
 import magentoImg from "../../../../../assets/image/integration/magento.png"
 import amazonImg from "../../../../../assets/image/logo/AmazonLogo.png"
 import amazonDirImg from "../../../../../assets/image/integration/AmazonLogo.png"
+import APIChannelIcon from "../../../../../assets/image/integration/APIChannelIcon.png"
 import customImg from "../../../../../assets/image/integration/Manual.png"
 import { weightCalculation, weightGreater } from '../../../../../customFunction/functionLogic';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
@@ -248,7 +249,8 @@ const Processing = React.memo(({ orders, activeTab, setOrderTagId, selectAll, se
                                                                         : row.channel.toLowerCase() === "magento" ? <img src={magentoImg} alt="Manual" width="20" />
                                                                             : row.channel.toLowerCase() === "amazon" ? <img src={amazonImg} alt="Manual" width="20" />
                                                                                 : row.channel.toLowerCase() === "amazondirect" ? <img src={amazonDirImg} alt="Manual" width="20" />
-                                                                                    : <CustomIcon />}
+                                                                                    : row.channel.toLowerCase() === "api" ? <img src={APIChannelIcon} alt="Manual" width="20" />
+                                                                                        : <CustomIcon />}
                                                         <span className='d-inline-flex align-items-center gap-1 ms-2'>
                                                             <Link to={`/orderdetail/${row?.id}`} className='anchor-order'>{row.customer_order_number}</Link>
                                                             {row?.other_details?.is_verified &&
