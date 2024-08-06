842075dc106f2d0d11a1151499fdf6d9f076c6b1
Shipease2 <himanshu.grover@shipease.in>
2024-08-06 11:16:14 +0530

    WhatsApp Integration UI Updated

--- src/Component/Pages/IntegrationsPage/Components/OtherIntegration/WhatsAppIntegration.js
+++ src/Component/Pages/IntegrationsPage/Components/OtherIntegration/WhatsAppIntegration.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import Logo from '../../../../../assets/image/integration/whatsappIcon.png';
+import HeartIcon from './HeartIcon';
+import TruckIcon from './TruckIcon';
+import PackageMagnifyingIcon from './PackageMagnifyingIcon';
 
 const WhatsAppIntegration = () => {
     return (
@@ -14,12 +17,27 @@ const WhatsAppIntegration = () => {
                         <p className='mb-0'>Need Help?</p>
                     </div>
                 </section>
-                <div className='box-shadow shadow-sm p7 mt-2'>
+                <div className='box-shadow shadow-sm p-3 mt-3'>
                     <h4>Instruction to integrate WhatsApp Communication</h4>
-                    <p>Send Live Updates to your buyers with <span>93%</span> read rate</p>
-                    <h5>&#8377; 0.99* <span>per message</span></h5>
-                    <button>Activate Now</button>
-                    <p><span>Note:</span> Customise real time tracking update to share with your buyers, per status just @ &#8377;0.99. To avail all statuses, pay just ₹6.93 per order. By Default all statuses will be selected. (Prices are exclusive of GST & Non refundable)</p>
+                    <p>Keep your buyers informed with live updates that have a <span>93%</span> read rate.</p>
+                    <h5 className='font24'>&#8377; 0.99 <span className='font12'>per message</span></h5>
+                    <button className='btn main-button'>Activate Now</button>
+                    <p className='font12 mt-2'><span className='fw-bold'>Note:</span> Customise real time tracking update to share with your buyers, per status just @ &#8377;0.99. Customize real-time tracking updates to share with your buyers for just ₹0.99 per status. Get all status updates for only ₹6.93 per order. By default, all statuses will be selected. Prices are exclusive of GST and non-refundable.</p>
+                    <h4>How WhatsApp Communication Benefits You</h4>
+                    <ul className='whatsapp-benefits'>
+                        <li><HeartIcon />Enhance customer satisfaction by providing real-time updates to buyers.</li>
+                        <li><TruckIcon />Facilitate timely and efficient deliveries with detailed brand information.</li>
+                        <li><PackageMagnifyingIcon />Reduce customer inquiries by increasing the visibility of shipment details.</li>
+                    </ul>
+
+                    <h4>Try It Out!</h4>
+                    <p>Enter your number to receive live tracking updates via WhatsApp.</p>
+                    <label className='whatsapp-get-started'>
+                        <input type="text" placeholder='Enter 10 Digit Mobile Number' />
+                        <button>Get Started</button>
+                    </label>
+
+                    <h4></h4>
                 </div>
 
             </div>
