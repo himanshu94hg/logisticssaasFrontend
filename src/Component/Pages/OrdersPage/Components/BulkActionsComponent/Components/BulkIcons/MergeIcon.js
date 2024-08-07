141459915d26ffe18f7b144c3accf5ea63b03c41
Shipease2 <himanshu.grover@shipease.in>
2024-08-07 16:49:19 +0530

    Bulk Icons Updated

--- src/Component/Pages/OrdersPage/Components/BulkActionsComponent/Components/BulkIcons/MergeIcon.js
+++ src/Component/Pages/OrdersPage/Components/BulkActionsComponent/Components/BulkIcons/MergeIcon.js
@@ -4,33 +4,27 @@ const MergeIcon = () => {
     return (
         <>
             <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
                 width={24}
                 height={24}
-                x={0}
-                y={0}
-                viewBox="0 0 32 32"
-                style={{ enableBackground: "new 0 0 512 512" }}
-                xmlSpace="preserve"
-                fillRule="evenodd"
-                className="hovered-paths"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
             >
-                <defs>
-                    <linearGradient id="mergeGradient" x1="0" x2="1" y1="0" y2="0">
-                        <stop offset="0.386" stopColor="#1975c9" stopOpacity="1" />
-                        <stop offset="1" stopColor="#c5dcf1" stopOpacity="1" />
-                    </linearGradient>
-                </defs>
-                <g>
+                <g clipPath="url(#clip0_1013_3202)">
                     <path
-                        d="M16 0C7.163 0 0 7.163 0 16s7.163 16 16 16 16-7.163 16-16S24.837 0 16 0zm3.707 18.707L16 21.414l-3.707-3.707 1.414-1.414L15 17.586V10h2v7.586l1.293-1.293 1.414 1.414zM10 8h12v2H10V8z"
-                        fill="url(#mergeGradient)"
-                        className="hovered-path"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 0C5.37225 0 0 5.37225 0 12C0 18.6277 5.37225 24 12 24C18.6277 24 24 18.6277 24 12C24 5.37225 18.6277 0 12 0ZM14.7803 14.0303L12 16.0605L9.21975 13.2803L10.2802 12.2197L11.25 13.1895V7.5H12.75V13.1895L13.7197 12.2197L14.7803 13.2803V14.0303ZM7.5 6H16.5V7.5H7.5V6Z"
+                        fill="#1975C9"
                     />
                 </g>
+                <defs>
+                    <clipPath id="clip0_1013_3202">
+                        <rect width={24} height={24} fill="white" />
+                    </clipPath>
+                </defs>
             </svg>
+
         </>
     )
 }
