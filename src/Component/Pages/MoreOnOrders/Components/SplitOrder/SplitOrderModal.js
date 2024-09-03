99cb1e39e21c462604c208e0175fc0f3b30609c2
HimanshuShipease <himanshu.grover@shipease.in>
2024-09-03 15:21:34 +0530

    Split Order Popup UI Updated

--- src/Component/Pages/MoreOnOrders/Components/SplitOrder/SplitOrderModal.js
+++ src/Component/Pages/MoreOnOrders/Components/SplitOrder/SplitOrderModal.js
@@ -5,6 +5,7 @@ import Modal from "react-bootstrap/Modal";
 import React, { useEffect, useState } from "react";
 import { BASE_URL_CORE, BASE_URL_ORDER } from "../../../../../axios/config";
 import { customErrorFunction } from '../../../../../customFunction/errorHandling';
+import { FaInfoCircle } from 'react-icons/fa';
 
 
 function SplitOrderModal({ show, handleClose, orderDetails, setSplitStatus }) {
@@ -59,61 +60,61 @@ function SplitOrderModal({ show, handleClose, orderDetails, setSplitStatus }) {
     };
 
     return (
-        <Modal show={show} onHide={handleClose}>
+        <Modal className="confirmation-modal split-order-popup" show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Split Order</Modal.Title>
             </Modal.Header>
-            <Modal.Body className='p-1'>
-                <div className="card">
-                    <div className="card-body">
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Sr.No</th>
-                                    <th>Item Name & SKU</th>
-                                    <th>Warehouse to Ship</th>
+            <Modal.Body>
+                <div className="mx-3">
+                    <table className="w-100">
+                        <thead>
+                            <tr>
+                                <th>Sr.No</th>
+                                <th>Item Name & SKU</th>
+                                <th>Warehouse to Ship</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orderDetails?.order_products.map((product, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{product.product_name}</td>
+                                    <td>
+                                        <select
+                                            className='select-field'
+                                            style={{ width: '100%', maxWidth: '200px' }}
+                                            value={selectedWarehouses[index]}
+                                            onChange={(e) => {
+                                                const newSelectedWarehouses = [...selectedWarehouses];
+                                                newSelectedWarehouses[index] = e.target.value;
+                                                setSelectedWarehouses(newSelectedWarehouses);
+                                            }}>
+                                            <option value="">Select Warehouse</option>
+                                            {warehouseData.map((warehouse, warehouseIndex) => (
+                                                <option
+                                                    key={warehouseIndex}
+                                                    value={warehouse.id}
+                                                >
+                                                    {warehouse.warehouse_name}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {orderDetails?.order_products.map((product, index) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{product.product_name}</td>
-                                        <td>
-                                            <select
-                                                className='select-field'
-                                                style={{ width: '100%' }}
-                                                value={selectedWarehouses[index]}
-                                                onChange={(e) => {
-                                                    const newSelectedWarehouses = [...selectedWarehouses];
-                                                    newSelectedWarehouses[index] = e.target.value;
-                                                    setSelectedWarehouses(newSelectedWarehouses);
-                                                }}>
-                                                <option value="">Select Warehouse</option>
-                                                {warehouseData.map((warehouse, warehouseIndex) => (
-                                                    <option
-                                                        key={warehouseIndex}
-                                                        value={warehouse.id}
-                                                    >
-                                                        {warehouse.warehouse_name}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </Modal.Body>
             <Modal.Footer>
-                <button className="btn btn-secondary" onClick={handleClose}>
-                    Cancel
-                </button>
-                <button className="btn main-button" onClick={handleSplitOrder} disabled={loading}>
-                    {loading ? 'Splitting...' : 'Split Order'}
-                </button>
+                <p className="font12 d-flex align-items-center gap-1">
+                    <FaInfoCircle color="#1975C9" className="font15" />
+                    Split will create custom orders.
+                </p>
+                <div className="d-flex gap-2">
+                    <button className="btn cancel-button" onClick={handleClose}>Cancel</button>
+                    <button className="btn main-button" onClick={handleSplitOrder} disabled={loading}>{loading ? 'Splitting...' : 'Continue'}</button>
+                </div>
             </Modal.Footer>
         </Modal>
     );
