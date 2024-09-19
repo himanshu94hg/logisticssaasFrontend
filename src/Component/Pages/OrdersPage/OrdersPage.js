7e7703ce8cc2c04722aeb57d5bb3c7716ce87ad8
HimanshuShipease <himanshu.grover@shipease.in>
2024-09-19 12:23:00 +0530

    Most popular search UI Updated

--- src/Component/Pages/OrdersPage/OrdersPage.js
+++ src/Component/Pages/OrdersPage/OrdersPage.js
@@ -162,7 +162,7 @@ const OrdersPage = () => {
                     apiUrl = `${BASE_URL_ORDER}/orders-api/orders/?courier_status=Unprocessable&page_size=${itemsPerPage}&page=${currentPage}&q=${sanitizedSearchValue}&search_by=${searchType}`;
                     break;
                 case "Processing":
-                    apiUrl = `${BASE_URL_ORDER}/orders-api/orders/?courier_status=Processing&page_size=${itemsPerPage}&page=${currentPage}&q=${sanitizedSearchValue}&search_by=${searchType}`;
+                    apiUrl = `${BASE_URL_ORDER}/orders-api/orders/?courier_status=Unprocessable&page_size=${itemsPerPage}&page=${currentPage}&q=${sanitizedSearchValue}&search_by=${searchType}`;
                     break;
                 case "Ready to Ship":
                     apiUrl = `${BASE_URL_ORDER}/orders-api/orders/?courier_status=Ready_to_ship&page_size=${itemsPerPage}&page=${currentPage}&q=${sanitizedSearchValue}&search_by=${searchType}`;
@@ -291,7 +291,7 @@ const OrdersPage = () => {
 
 
     const handleSearchKey = (e) => {
-        if(e.key==="Enter"){
+        if (e.key === "Enter") {
             setReset(new Date())
         }
         const allowedCharacters = /^[a-zA-Z0-9\s!@#$%^&*(),.?":{}|<>]*$/;
@@ -371,14 +371,16 @@ const OrdersPage = () => {
 
 
                     </div>
-                    <p className='font10 mt-1'>Most Popular Search by
-                        <span> COD</span> |
-                        <span>Prepaid</span> |
-                        <span>Yesterday</span> |
-                        <span>One Week</span> |
-                        <span>Last Month</span> |
-                        <span>Delivered</span> |
-                        <span>Cancel order</span> </p>
+                    <p className='popular-search'>Most Popular Search by
+                        <span className='text-sh-red'> Info Missing</span> |
+                        <span className="text-green"> Live</span> |
+                        <span className='text-sh-primary'> COD</span> |
+                        <span className='text-sh-primary'> Prepaid</span> |
+                        <span className='text-sh-primary'> Yesterday</span> |
+                        <span className='text-yellow'> One Week</span> |
+                        <span> Last Month</span> |
+                        <span className='text-green'> Delivered</span> |
+                        <span className='text-sh-red'> Cancel order</span> </p>
                 </div>
                 {screenWidthData < 592 &&
                     <div className="nav-actions-container">
