3b665fedaf5ae12ed27d2f2941916f5092809864
HimanshuShipease <himanshu.grover@shipease.in>
2024-09-23 18:06:37 +0530

    Tracking Script UI Updated

--- src/Component/Pages/SettingsPage/components/TrackingScript/TrackingScript.js
+++ src/Component/Pages/SettingsPage/components/TrackingScript/TrackingScript.js
@@ -12,7 +12,7 @@ const TrackingScript = () => {
 
                 <div className='mt-4'>
                     <h6 className='fw-bold'>Add this code in the Footer of the web page</h6>
-                    <textarea placeholder='Click on Generate Script to get this script...' className='input-field tracking-code-textarea' name="" id=""></textarea>
+                    <textarea placeholder='Click on Generate Script to get this script...' disabled className='input-field tracking-code-textarea' name="" id=""></textarea>
                     <div className='d-flex justify-content-end w-100 mt-4'>
                         <button className='btn main-button'>Copy Tracking Code</button>
                     </div>
