ea5a206066fa7540974aecfd08cbee465c54eeee
HimanshuShipease <himanshu.grover@shipease.in>
2024-12-26 15:27:40 +0530

    VAS Rates Page UI Updated

--- src/Component/Pages/ToolsPage/Components/RateCard/VASRates/VASRates.js
+++ src/Component/Pages/ToolsPage/Components/RateCard/VASRates/VASRates.js
@@ -29,56 +29,56 @@ const VASRates = () => {
                             <WhatsAppVASIcon />
                             <div className="card-text-box">
                                 <p className="card-title">WhatsApp</p>
-                                <p className="card-price">₹ 0.99 per message</p>
+                                <p className="card-price">₹ {isChecked ? (0.99 * 1.18).toFixed(2) : 0.99} per message</p>
                                 <img
                                     src="https://via.placeholder.com/150"
                                     alt="WhatsApp"
                                     className="card-image"
                                 />
-                                <p className="card-price">Tracking: ₹ 1 per message</p>
-                                <p className="card-price">Bot: ₹ 2.99 per flow</p>
+                                <p className="card-price">Tracking: ₹ {isChecked ? 1 * 1.18 : 1} per message</p>
+                                <p className="card-price">Bot: ₹ {isChecked ? (2.99 * 1.18).toFixed(2) : 2.99} per flow</p>
                             </div>
                         </div>
                         <div className="vas-card-container">
                             <NDRVasIcon />
                             <div className="card-text-box">
                                 <p className="card-title">NDR</p>
-                                <p className="card-price">₹ 0.99 per call</p>
+                                <p className="card-price">₹ {isChecked ? (0.99 * 1.18).toFixed(2) : 0.99} per call</p>
                                 <img
                                     src="https://via.placeholder.com/150"
                                     alt="WhatsApp"
                                     className="card-image"
                                 />
-                                <p className="card-price invisible">IVR: ₹ 0.49 per call</p>
-                                <p className="card-price">IVR: ₹ 0.49 per call</p>
+                                <p className="card-price invisible">IVR: ₹ {isChecked ? (0.49 * 1.18).toFixed(2) : 0.49} per call</p>
+                                <p className="card-price">IVR: ₹ {isChecked ? (0.49 * 1.18).toFixed(2) : 0.49} per call</p>
                             </div>
                         </div>
                         <div className="vas-card-container">
                             <EmailVasIcon />
                             <div className="card-text-box">
                                 <p className="card-title">Email</p>
-                                <p className="card-price">₹ 0.49 per order</p>
+                                <p className="card-price">₹ {isChecked ? (0.49 * 1.18).toFixed(2) : 0.49} per order</p>
                                 <img
                                     src="https://via.placeholder.com/150"
                                     alt="WhatsApp"
                                     className="card-image"
                                 />
-                                <p className="card-price invisible">Tracking: ₹ 1 /message</p>
-                                <p className="card-price invisible">Tracking: ₹ 1 /message</p>
+                                <p className="card-price invisible">Tracking: ₹ {isChecked ? 1 * 1.18 : 1} /message</p>
+                                <p className="card-price invisible">Tracking: ₹ {isChecked ? 1 * 1.18 : 1} /message</p>
                             </div>
                         </div>
                         <div className="vas-card-container">
                             <SmsVasIcon />
                             <div className="card-text-box">
                                 <p className="card-title">SMS</p>
-                                <p className="card-price">₹ 0.25 per message</p>
+                                <p className="card-price">₹ {isChecked ? (0.25 * 1.18).toFixed(2) : 0.25} per message</p>
                                 <img
                                     src="https://via.placeholder.com/150"
                                     alt="WhatsApp"
                                     className="card-image"
                                 />
-                                <p className="card-price invisible">SMS: ₹ 0.99 per order</p>
-                                <p className="card-price">SMS: ₹ 0.99 per order</p>
+                                <p className="card-price invisible">SMS: ₹ {isChecked ? (0.99 * 1.18).toFixed(2) : 0.99} per order</p>
+                                <p className="card-price">SMS: ₹ {isChecked ? (0.99 * 1.18).toFixed(2) : 0.99} per order</p>
                             </div>
                         </div>
                         <div className="vas-card-container">
@@ -92,7 +92,7 @@ const VASRates = () => {
                                     alt="WhatsApp"
                                     className="card-image"
                                 />
-                                <p className="card-price invisible">Bot: ₹ 1 /message</p>
+                                <p className="card-price invisible">Bot: ₹ {isChecked ? (1 * 1.18).toFixed(2) : 1} /message</p>
                                 <p className="card-price">All RAC rates given</p>
                             </div>
                         </div>
